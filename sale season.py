ba9c149d1501aa128dcf2deeba84bc6a41180371
Charanpreet Singh Jaswal <99860377+chrnpreetsingh@users.noreply.github.com>
2022-08-13 14:56:46 +0530

    Create sale season.py

---
+++ sale season.py
@@ -0,0 +1,10 @@
+for i in range(int(input())):
+    x = int(input())
+    if x <= 100:
+        print(x)
+    elif x > 100 and x <= 1000:
+        print(x - 25)
+    elif x > 1000 and x <= 5000:
+        print(x - 100)
+    elif x > 5000:
+        print(x-500)
